8787596eb7e2d4ef6395ce778b43d746ff89d92f
Tajbir islam <studenttajbirislam@gmail.com>
2024-05-13 20:22:56 +0600

    fix some bugs

--- src/pages/Borrowed.jsx
+++ src/pages/Borrowed.jsx
@@ -44,19 +44,15 @@ const Borrowed = () => {
     }
 
     if(user?.email === undefined){
-        return (
-          <div className="md:h-96 h-40 flex items-center justify-center">
-            <span className="loading loading-spinner loading-lg"></span>
-          </div>
-        );
-      }
+      window.location.reload()
+    }
   return (
  
     <div className="mb-20">
     <Helmet>
       <title>Borrowed Books</title>
     </Helmet>
-        <h1 className="md:text-4xl text-xl font-bold text-center mb-10">Popular Book</h1>
+        <h1 className="md:text-4xl text-xl font-bold text-center mb-10">Borrowed Book</h1>
         <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 xl:gap-32 lg:gap-10 md:gap-14 gap-10">
         {book.map((items) => {
             const {borrowId} = items
